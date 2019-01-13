31516c93cb8efa84093c4447697097b457cab8ae
Alexandr <als.kozlov@yandex.ru>
2019-01-13 13:39:36 +0300

    chat

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.css.PseudoClass;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller extends Dialog {
+
+
+
+
+    public void sendMsg(ActionEvent event){
+
+    }
+}
